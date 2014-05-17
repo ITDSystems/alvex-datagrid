e2c116be8688c7b42e11eb91e51018dea7906767
Alexey Vasyukov <vasyukov@gmail.com>
2014-05-17 22:29:11 +0400

    Working implementation.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-alvex-datagrid
+Alvex Datagrid
 ==============
 
-Generic purpose datagrid enhancements
+Generic purpose datagrid enhancements extracted from Alvex sources and refactored for Alfresco Maven SDK.
---
+++ alvex-datagrid-share/src/main/amp/web/scripts/components/alvex/datalists/datagrid-search.js
@@ -0,0 +1,211 @@
+/**
+ * Copyright (C) 2014 ITD Systems LLC.
+ *
+ * This file is part of Alvex
+ *
+ * Alvex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alvex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alvex. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Ensure root object exists
+if (typeof Alvex === "undefined" || !Alvex)
+{
+   var Alvex = {};
+}
+
+/**
+ * Data Lists: DataGrid component.
+ * 
+ * @namespace Alvex
+ * @class Alvex.DataGridSearch
+ */
+(function()
+{
+   var $html = Alfresco.util.encodeHTML;
+   var $func = Alvex.util.getFunctionByName;
+   
+   Alvex.DataGridSearch = {};
+   Alvex.DataGridSearch.prototype =
+   {
+      defaultSearchRenderersNames: {},
+	  
+      /**
+       * Render search form
+       */
+      renderSearch: function DataGrid_renderSearch(response)
+      {
+         // TODO - move it out of renderSearch() and make configurable
+         this.defaultSearchRenderersNames["default"] = "Alvex.DatagridTextSearchRenderer";
+         this.defaultSearchRenderersNames["text"] = "Alvex.DatagridTextSearchRenderer";
+         this.defaultSearchRenderersNames["mltext"] = "Alvex.DatagridTextSearchRenderer";
+         this.defaultSearchRenderersNames["select"] = "Alvex.DatagridSelectSearchRenderer";
+         this.defaultSearchRenderersNames["date"] = "Alvex.DatagridDateRangeSearchRenderer";
+         this.defaultSearchRenderersNames["datetime"] = "Alvex.DatagridDateRangeSearchRenderer";
+         this.defaultSearchRenderersNames["cm:person"] = "Alvex.DatagridTextSearchRenderer";
+         this.defaultSearchRenderersNames["association"] = "Alvex.DatagridTextSearchRenderer";
+
+         if( ! Dom.get(this.id + '-search') )
+            return;
+
+         if( !this.datalistColumns.length || this.datalistColumns.length === 0 )
+            return;
+		
+         Dom.removeClass(this.id + "-search-container", "hidden");
+
+         // Get constraints for fields in question
+         // WA: very-very long form description to parse
+         var text = response.serverResponse.responseText;
+         var constraints = text.replace(/[\s\S]*fieldConstraints:/, "").replace(/\}\)\.setMessages\([\s\S]*/, "");
+         var json = eval('(' + constraints + ')');
+         for(var i in json)
+            this.datalistColumnsConstraints[ json[i].fieldId.replace(/^tmp_/,"") ] = json[i];
+
+         // Clear everything
+         if( this.widgets.searchForm )
+            delete this.widgets.searchForm;
+         Dom.get(this.id + '-search').innerHTML = '';
+
+         // Render table to hold search field
+         var outerEl = this.widgets.dataTable.getColumn('nodeRef').getThEl();
+         var width = Number(outerEl.offsetWidth) - 4;
+         var row = '<td id="' + this.id + '-search-span-' + 'nodeRef' + '-c" class="datagrid-search-field" ' 
+                   + 'style="min-width:' + width + 'px;">&nbsp;</td>';
+
+         for( var col = 0; col < this.datalistColumns.length; col++ )
+         {
+            var key = this.dataResponseFields[col];
+            outerEl = this.widgets.dataTable.getColumn(key).getThEl();
+            width = Number(outerEl.offsetWidth) - 4;
+            row += '<td id="' + this.id + '-search-span-' + key + '-c" class="datagrid-search-field" ' 
+                   + ' style="min-width:' + width + 'px;"></td>';
+         }
+         row += '<td id="' + this.id + '-search-span-' + 'actions' + '-c" style="min-width:90px;" class="datagrid-search-field">' 
+                  + '<span class="small-btn" id="' + this.id + '-search-span-' + 'actions-search' + '"></span>'
+                  + '<span class="small-btn" id="' + this.id + '-search-span-' + 'actions-clear' + '"></span>'
+                  + '</td>';
+         Dom.get(this.id + "-search").innerHTML = row;
+
+         // Render search fields themselves
+         for( var col = 0; col < this.datalistColumns.length; col++ )
+         {
+            var key = this.dataResponseFields[col];
+            
+            var datalistColumn = this.datalistColumns[col];
+            var renderer = null;
+            var datatype = "";
+            var availableOptions = null;
+
+            // List constraint is a special case, set custom datatype value
+            if( this.datalistColumnsConstraints[key] 
+                 && ( this.datalistColumnsConstraints[key].handler === Alfresco.forms.validation.inList ) )
+            {
+               datatype = "select";
+               availableOptions = this.datalistColumnsConstraints[key].params.allowedValues;
+            // Other cases - just determine datatype
+            } else {
+               datatype = datalistColumn.dataType.toLowerCase();
+            }
+
+            // Find available renderer
+            if(typeof $func(this.defaultSearchRenderersNames[datatype]) === "function")
+               renderer = $func(this.defaultSearchRenderersNames[datatype]);
+            else if( datalistColumn.type === "association" )
+               renderer = $func(this.defaultSearchRenderersNames["association"]);
+            else
+               renderer = $func(this.defaultSearchRenderersNames["default"]);
+
+            // Get current value of this field from saved search
+            var curValue = this.savedSearch[key] ? this.savedSearch[key] : "";
+
+            // Render field
+            var searchFieldHtmlId = this.id + '-search-span-' + key; 
+            renderer.call(this, searchFieldHtmlId, key, curValue, availableOptions);
+         }
+
+         var me = this;
+         // Create search form inlined buttons
+         var oSearchButton = new YAHOO.widget.Button({ type: "push", label: '<span class="datagrid-search-button"></span>', 
+                                                        container: this.id + '-search-span-' + 'actions-search', 
+														onclick: { fn: me.doSearch, scope: me } });
+
+         var oClearButton = new YAHOO.widget.Button({ type: "push", label: '<span class="datagrid-clear-search-button"></span>', 
+                                                        container: this.id + '-search-span-' + 'actions-clear',
+                                                        onclick: { fn: me.clearSearch, scope: me } });
+
+         // Call search when any search field changes
+         for( var col = 0; col < this.datalistColumns.length; col++ )
+         {
+            var key = this.dataResponseFields[col];
+			var searchFieldHtmlId = this.id + '-search-span-' + key; 
+            var el = Dom.get( searchFieldHtmlId );
+            if( el !== null )
+            {
+               el.onchange = function()
+                  {
+                     me.doSearch();
+                  };
+            }
+         }
+      },
+
+      clearSearch: function()
+      {
+         this.savedSearch = {};
+         this._updateDataGrid({ filter: { eventGroup: this, filterId: "all", filterData: "" } });
+      },
+
+      onDataGridResize: function()
+      {
+         this.resizeSearch();
+      },
+
+      resizeSearch: function()
+      {
+         for( var col = 0; col < this.datalistColumns.length; col++ )
+         {
+            var key = this.dataResponseFields[col];
+            var outerEl = this.widgets.dataTable.getColumn(key).getThEl();
+            var outerWidth = outerEl.offsetWidth;
+            var el = Dom.get( this.id + '-search-span-' + key );
+            el.style.minWidth = (Number(outerWidth)-4) + 'px';
+         }
+      },
+
+      doSearch: function(layer, args)
+      {
+         var config = {};
+         config.dataObj = {};
+         config.url = Alfresco.constants.PROXY_URI
+                 + "api/alvex/datalists/search/node/" + Alfresco.util.NodeRef( this.datalistMeta.nodeRef ).uri;
+         config.dataObj.fields = this.dataRequestFields;
+         config.dataObj.filter = {eventGroup: this, filterId: "search", filterData: "", searchFields: { props: {}, assocs: {} }};
+         //for(var i in config.dataObj) {
+         for( var col = 0; col < this.datalistColumns.length; col++ )
+         {
+            var key = this.dataResponseFields[col];
+			var searchFieldHtmlId = this.id + '-search-span-' + key; 
+            var val = Dom.get(searchFieldHtmlId).value;
+            if( key.match(/^prop_/) ) {
+               this.savedSearch[key] = val.replace('"','\\"');
+               config.dataObj.filter.searchFields.props[key.replace(/^prop_/, '')] = val;
+            } else if( key.match(/^assoc_/) ) {
+               this.savedSearch[key] = val.replace('"','\\"');
+               config.dataObj.filter.searchFields.assocs[key.replace(/^assoc_/, '')] = val;
+            }
+         }
+         this._updateDataGrid(config.dataObj);
+         // Prevent extra submission - everything should be handled by dataSource in _updateDataGrid call
+         return false;
+      }
+   };
+})();
